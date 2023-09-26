1158e2adb6dc487973077c764d51f346d63b336c
Luis B <luis.beroiza@hotmail.com>
2023-09-26 16:17:32 +0200

    Agrega nuevo método Rest y variable en el application.properties usando anotaciones de JEE standard

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -2,15 +2,30 @@ package org.acme;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class ExampleResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
-    }
+  @ConfigProperty(name = "ExampleResource.greeting")
+  String greeting;
+
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello from RESTEasy Reactive";
+  }
+
+
+  @GET
+  @Path("/{name}")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String customGreeting(@PathParam("name") String name) {
+    String question = "How are you doing?";
+    return String.format("%s, %s. %s", greeting, name, question);
+  }
 }
